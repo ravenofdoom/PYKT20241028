2570020d2e356528f22101b264c3a52ed1893c2d
stkokt <stefan.koschnik@mein-karrieretutor.de>
2024-11-05 12:06:44 +0100

    Create Aufgaben LG1 mit Lsg.py

---
+++ Aufgaben LG1 mit Lsg.py
@@ -0,0 +1,69 @@
+# Aufgabe 1: Addition zweier Zahlen
+
+a = 5
+b = 3
+ergebnis = a + b
+print(ergebnis)
+
+
+# Aufgabe 2: Subtraktion zweier Zahlen
+
+a = 10
+b = 4
+ergebnis = a - b
+print(ergebnis)
+
+# Aufgabe 3: Multiplikation zweier Zahlen
+
+a = 7
+b = 2
+ergebnis = a * b
+print(ergebnis)
+
+# Aufgabe 4: Division zweier Zahlen
+
+a = 20
+b = 5
+ergebnis = a / b
+print(ergebnis)
+
+# Aufgabe 5: Berechnung des Durchschnitts von drei Zahlen
+
+a = 8
+b = 12
+c = 15
+durchschnitt = (a + b + c) / 3
+print(durchschnitt)
+
+# Aufgabe 6: Berechnung des Flächeninhalts eines Rechtecks
+
+laenge = 10
+breite = 5
+flaeche = laenge * breite
+print(flaeche)
+
+# Aufgabe 7: Berechnung des Umfangs eines Rechtecks
+
+laenge = 10
+breite = 5
+umfang = 2 * (laenge + breite)
+print(umfang)
+
+# Aufgabe 8: Berechnung des Flächeninhalts eines Dreiecks
+
+basis = 6
+hoehe = 8
+flaeche = 0.5 * basis * hoehe
+print(flaeche)
+
+# Aufgabe 9: Berechnung des Umfangs eines Kreises
+
+radius = 7
+umfang = 2 * 3.14159 * radius
+print(umfang)
+
+# Aufgabe 10: Berechnung des Flächeninhalts eines Kreises
+
+radius = 7
+flaeche = 3.14159 * radius * radius
+print(flaeche)
