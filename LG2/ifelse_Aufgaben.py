04f28709f43d526285626c3d31096557b468ecb1
stkokt <stefan.koschnik@mein-karrieretutor.de>
2024-11-07 09:32:59 +0100

    IfElse

---
+++ LG2/ifelse_Aufgaben.py
@@ -0,0 +1,76 @@
+# 1. Schreibe ein Programm, das eine Zahl von der Eingabe liest und ausgibt, 
+# ob die Zahl positiv, negativ oder null ist.
+
+print("Aufgabe 1\n")
+
+
+# 2. Schreibe ein Programm, das eine Zahl von der Eingabe liest und ausgibt, 
+# ob die Zahl gerade oder ungerade ist.
+
+print("Aufgabe 2\n")
+
+
+
+# 3. Schreibe ein Programm, das eine Zahl von der Eingabe liest und ausgibt, 
+# ob die Zahl im Bereich von 1 bis 10 liegt.
+
+print("Aufgabe 3\n")
+
+
+
+# 4. Schreibe ein Programm, das drei Zahlen von der Eingabe liest 
+# und die größte der drei Zahlen ausgibt.
+
+print("Aufgabe 4\n")
+
+
+
+
+# 5. Schreibe ein Programm, das eine Zahl (1-7) von der Eingabe liest 
+# und den entsprechenden Wochentag ausgibt 
+# (1: Montag, 2: Dienstag, ..., 7: Sonntag).
+
+print("Aufgabe 5")
+
+
+
+
+# 6. Schreibe ein Programm, das eine Zahl von der Eingabe liest und ausgibt, 
+# ob die Zahl durch 2, 3 oder 5 teilbar ist.
+
+
+
+
+# 7. Schreibe ein Programm, das zwei Strings von der Eingabe liest und ausgibt, 
+# ob die Strings gleich sind, der erste String länger ist oder 
+# der zweite String länger ist.
+
+print("Aufgabe 7")
+
+
+
+
+# 8. Schreibe ein Programm, das einen String und einen Buchstaben von der 
+# Eingabe liest und ausgibt, ob der Buchstabe im String enthalten ist, 
+# nicht enthalten ist oder der String leer ist.
+
+print("Aufgabe 8")
+
+
+
+
+# 9. Schreibe ein Programm, das einen String von der Eingabe liest und ausgibt, 
+# ob der String nur aus Buchstaben, nur aus Zahlen oder 
+# aus einer Mischung von beiden besteht.
+
+print("Aufgabe 9")
+
+
+
+
+# 10. Schreibe ein Programm, das einen String von der Eingabe liest und ausgibt, 
+# ob der String mit einem bestimmten Suffix (hinterster Teilstring) endet.
+
+print("Aufgabe 10")
+
+
