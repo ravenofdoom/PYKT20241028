53101baf99b317b2ed347e6f315f841739385547
stkokt <stefan.koschnik@mein-karrieretutor.de>
2024-12-06 13:19:46 +0100

    Modularisierung

--- Modularisiert/main.py
+++ Modularisiert/main.py
@@ -10,4 +10,8 @@ Code
 
 import modul
 
+# Das Modul führt jetzt den Code aus,
+# der dort über dem If- Block steht
+
+# Anschließend:
 print(f"ICH BIN DIE MAIN !!!",{__name__})
--- Modularisiert/modul.py
+++ Modularisiert/modul.py
@@ -1,4 +1,6 @@
 # modul
 print(f"KnockKnock from {__name__}")
+
+# Dieser Block wird nur ausgeführt, wenn diese Datei direkt ausgeführt wird
 if __name__ == "__main__":
     print(f"Hello from {__name__}")
